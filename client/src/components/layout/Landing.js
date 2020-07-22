da5fa18b50ac67d19f8c60cf0bca10ef84fe91d0
chrishennessy <58816114+chrishennessy@users.noreply.github.com>
2020-07-22 14:12:16 +0800

    Add div to landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import bdlogo from '../../static/bdlogo.png';
@@ -11,17 +11,25 @@ const Landing = () => (
     className='d-flex flex-column col-6 justify-content-center align-items-center'
   >
     <Row>
-      <p className='mb-4 text-secondary h1'> Welcome to Bonus Dynamo</p>
-      <p className='mb-4 text-secondary h4'>
-        Rapidly Create Optimized Bonus Pages On The Fly!
-      </p>
-      <img src={bdlogo} alt='mongo' className='img-fluid mb-4' />
-      <p className='mb-4 text-secondary h3'>
-        Don't have an account? <Link to={'/signup'}>Sign Up!</Link>
-      </p>
-      <p className='mb-4 text-secondary p'>
-        Already have an account? <Link to={'/login'}>Login.</Link>
-      </p>
+      <Col className='text-center'>
+        <div>
+          <p className='mb-4 text-secondary h1'> Welcome to Bonus Dynamo</p>
+          <p className='mb-4 text-secondary h4'>
+            Rapidly Create Optimized Bonus Pages On The Fly!
+          </p>
+        </div>
+        <div className='text-center'>
+          <Image src={bdlogo} alt='mongo' className='mb-4' />
+        </div>
+        <div>
+          <p className='mb-4 text-secondary h3'>
+            Don't have an account? <Link to={'/signup'}>Sign Up!</Link>
+          </p>
+          <p className='mb-4 text-secondary p'>
+            Already have an account? <Link to={'/login'}>Login.</Link>
+          </p>
+        </div>
+      </Col>
     </Row>
   </Container>
 );
